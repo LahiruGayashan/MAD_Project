4135f9ba4943b3b1c31aca155afaa969af4b3143
KY LINE <72918623+Lahiru9988@users.noreply.github.com>
2022-05-11 01:44:04 +0530

    create_sing_in_user

--- app/src/main/java/com/example/madproject/import_3.java
+++ app/src/main/java/com/example/madproject/import_3.java
@@ -14,5 +14,18 @@ public class import_3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import3);
+
+        button = (Button) findViewById(R.id.button6);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(import_3.this,sing_in_user.class);
+                startActivity(intent);
+            }
+
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/madproject/sing_in_user.java
@@ -0,0 +1,18 @@
+package com.example.madproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+
+public class sing_in_user extends AppCompatActivity {
+    public Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_in_user);
+    }
+}
