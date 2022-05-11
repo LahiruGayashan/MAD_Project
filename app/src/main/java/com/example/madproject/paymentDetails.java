be9719084496e70675e67bf6f0723d3d54315ea5
KY LINE <72918623+Lahiru9988@users.noreply.github.com>
2022-05-11 13:25:49 +0530

    create PaymentDetails_page

--- app/src/main/java/com/example/madproject/paymentDetails.java
+++ app/src/main/java/com/example/madproject/paymentDetails.java
@@ -3,18 +3,12 @@ package com.example.madproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class payment extends AppCompatActivity {
+public class paymentDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payment);
-
-
-
-
-
+        setContentView(R.layout.activity_payment_details);
     }
 }
